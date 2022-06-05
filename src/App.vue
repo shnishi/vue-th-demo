485bfc96ead367ecf18bafac94d03cfd5c5c3b88
shnishi <shigeonishi@hotmail.com>
2022-06-05 16:16:01 -0500

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,21 @@
 
 <dl class="row" id="av-list">
   <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  
+  
+  
+<dd class="col-sm-9">
+<span>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 11V8l5 4-5 4v-3H1v-2h9zm-7.542 4h2.124A8.003 8.003 0 0 0 20 12 8 8 0 0 0 4.582 9H2.458C3.732 4.943 7.522 2 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10c-4.478 0-8.268-2.943-9.542-7z" fill="rgba(230,23,23,1)"/></svg>
+</span>
+
+<span> User <b class="text-danger">bruce3397@some.com</b></span>
+  has logged-in 3.5 times higher than normal users
+  
+  
   <dd class="col-sm-9"><span> User <b class="text-danger">bruce3397@some.com</b></span>
   has logged-in 3.5 times higher than normal users
-  <img src="">
-
+  
   </dd>
 
   <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
