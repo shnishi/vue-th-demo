48036c5c0d57201c5441df665180e4f186a0580c
shnishi <shigeonishi@hotmail.com>
2022-06-05 15:54:56 -0500

    3 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <h1>Anomalies and violations</h1>
+<p>
+
+<dl class="row" id="av-list">
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User <b class="text-danger">bruce3397@some.com</b></span>
+  has logged-in 3.5 times higher than normal users
+  <img src="">
+
+  </dd>
+
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+  <dt class="col-sm-3 font-italic small text-black-50">01-02-2022 01:00:32</dt>
+  <dd class="col-sm-9"><span> User access <b class="text-danger">bruce3397@some.com</b></span></dd>
+  
+
+</dl>
   </div>
 </template>
 
@@ -18,11 +67,20 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+#av-list {
+  height: 200px;
+  overflow: auto;
+}
+
+
+dt.col-sm-3 {
+  font-size: 10px;
+}
+
+dd.col-sm-9 {
+  font-size: 12px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-const { createApp } = require('vue');
 import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
+const { createApp } = require('vue');
 createApp(App).mount("#app");
